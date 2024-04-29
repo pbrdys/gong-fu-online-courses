2c9fa554c4938244ae96489bc517fba9da36804a
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-04-29 14:07:22 +0200

    Create course_detail view

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.course_overview.as_view(), name='course_overview'),
+    path('<slug:slug>/', views.course_detail, name='course_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Course
 
@@ -6,3 +6,16 @@ from .models import Course
 class course_overview(generic.ListView):
     queryset = Course.objects.all()
     template_name = "course_overview.html"
+    
+    
+def course_detail(request, slug):
+    queryset = Course.objects.all()
+    course = get_object_or_404(queryset, slug=slug)
+    
+    return render(
+        request,
+        "course_detail.html",
+        {
+            "course": course
+        }
+    )
