4fc46736dfb55d8d2423dd7c8594cff1550c4e53
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-05-16 22:26:41 +0200

    Implement access-denied for course-views

--- courses/views.py
+++ courses/views.py
@@ -4,6 +4,9 @@ from .models import Course
 from .forms import CourseForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.utils.decorators import method_decorator
+from custom_pages.views import user_has_permission, access_denied
 
 # Create your views here.
 class course_overview(generic.ListView):
@@ -12,67 +15,78 @@ class course_overview(generic.ListView):
     
     
 def course_detail(request, slug):
-    queryset = Course.objects.all()
-    course = get_object_or_404(queryset, slug=slug)
-    lessons = course.lessons.all()
-    
-    return render(
-        request,
-        "course_detail.html",
-        {
-            "course": course,
-            "lessons": lessons,
-        }
-    )
+    if user_has_permission(request.user):
+        queryset = Course.objects.all()
+        course = get_object_or_404(queryset, slug=slug)
+        lessons = course.lessons.all()
+        
+        return render(
+            request,
+            "course_detail.html",
+            {
+                "course": course,
+                "lessons": lessons,
+            }
+        )
+    else:
+        return access_denied(request)
     
     
 def course_add(request):
-    # Handle the POST request from the course form
-    if request.method == "POST":
-        course_form = CourseForm(data=request.POST)
-        if course_form.is_valid():
-            course = course_form.save(commit=False)
-            course.save()
-            # display success message string:
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Course successfully added'
-            )
-            
-    course_form = CourseForm()
-    
-    return render(
-        request,
-        "course_add.html",
-        {
-            "course_form": course_form,
-        }
-    )
+    if user_has_permission(request.user):
+        # Handle the POST request from the course form
+        if request.method == "POST":
+            course_form = CourseForm(data=request.POST)
+            if course_form.is_valid():
+                course = course_form.save(commit=False)
+                course.save()
+                # display success message string:
+                messages.add_message(
+                    request, messages.SUCCESS,
+                    'Course successfully added'
+                )
+                
+        course_form = CourseForm()
+        
+        return render(
+            request,
+            "course_add.html",
+            {
+                "course_form": course_form,
+            }
+        )
+    else:
+        return access_denied(request)
     
 def course_edit(request, course_id):
-    course = get_object_or_404(Course, pk=course_id)
-    if request.method == "POST":
-        course_form = CourseForm(data=request.POST, instance=course)
-        
-        if course_form.is_valid():
-            course_form.save()
-            messages.add_message(request, messages.SUCCESS, 'Course Updated!')
-            return HttpResponseRedirect(reverse('course_overview'))
+    if user_has_permission(request.user):
+        course = get_object_or_404(Course, pk=course_id)
+        if request.method == "POST":
+            course_form = CourseForm(data=request.POST, instance=course)
+            
+            if course_form.is_valid():
+                course_form.save()
+                messages.add_message(request, messages.SUCCESS, 'Course Updated!')
+                return HttpResponseRedirect(reverse('course_overview'))
+            else:
+                messages.add_message(request, messages.ERROR, 'Error updating course!')
         else:
-            messages.add_message(request, messages.ERROR, 'Error updating course!')
-    else:
-        # If it's a GET request, initialize the form with the instance data
-        course_form = CourseForm(instance=course)
+            # If it's a GET request, initialize the form with the instance data
+            course_form = CourseForm(instance=course)
 
-    return render(request, 'course_edit.html', {'course_edit_form': course_form, 'course': course})
-        
+        return render(request, 'course_edit.html', {'course_edit_form': course_form, 'course': course})
+    else:
+        return access_denied(request) 
     
     
     
 def course_delete(request, course_id):
-    course = get_object_or_404(Course, pk=course_id)
-    
-    course.delete()
-    messages.add_message(request, messages.SUCCESS, 'Course deleted!')
-    
-    return HttpResponseRedirect(reverse("course_overview"))
+    if user_has_permission(request.user):
+        course = get_object_or_404(Course, pk=course_id)
+        
+        course.delete()
+        messages.add_message(request, messages.SUCCESS, 'Course deleted!')
+        
+        return HttpResponseRedirect(reverse("course_overview"))
+    else:
+        return access_denied(request)
