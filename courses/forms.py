d88e66ef62893008d37045c8b4a0b954a6e60f07
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-05-27 21:40:23 +0200

    Implement Course Forms Tests

--- courses/forms.py
+++ courses/forms.py
@@ -5,7 +5,12 @@ class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ('title', 'slug', 'description', 'category', 'featured_image', 'content', 'level', 'order')
-    
+        error_messages = {
+            '__all__': {
+                'required': "This field is required. LOL",
+            },
+        }
+     
 class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
