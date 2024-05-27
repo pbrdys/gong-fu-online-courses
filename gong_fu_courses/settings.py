91419977f6543ef37230a01abbd16b4d591e4360
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-05-27 22:40:44 +0200

    Update Lesson Detail View

--- gong_fu_courses/settings.py
+++ gong_fu_courses/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-_7)1fbf-fq@nwrz$lw6bipgutntfbw1=hhr#u7avf^ipkkrcib'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
